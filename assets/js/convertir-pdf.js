efb15f5eb2306679cfa87fc4bc2bedc9dca5fd33
Agustin Gomez <agustingomez1986@hotmail.com>
2022-08-19 08:43:39 -0300

    Mejora de código PDF

--- assets/js/convertir-pdf.js
+++ assets/js/convertir-pdf.js
@@ -1,12 +1,16 @@
-const curriculoPDF = document.querySelector("#banner__redes__curriculo");
-curriculoPDF.addEventListener("click", async function(){
-    var downloading = document.body;
-    var doc = new jsPDF('p', 'pt', [1000, 3500]);
-    await html2canvas(downloading, {
-        allowTaint: true,
-        useCORS: true,
-    }).then((canvas)=>{
-        doc.addImage(canvas.toDataURL("image/jpg"), 'JPG', 0, 0, 1000, 3500);
+document.addEventListener("DOMContentLoaded", ()=>{
+    const $boton = document.querySelector("#banner__redes__curriculo");
+    $boton.addEventListener("click", ()=>{
+        const $elementoParaConvertir = document.body;
+        html2pdf()
+            .set({
+                filename: 'Agustin_Gomez_Portfolio.pdf',
+                image: {type: 'jpeg', quality: 1},
+                html2canvas: {scale: 1.4, letterRendering: true},
+                jsPDF: {unit: 'px', format: [1200, 4790], orientation: 'portrait'}
+            })
+            .from($elementoParaConvertir)
+            .save()
+            .catch(err=>console.log(err));
     });
-    doc.save("Agustin_Gomez_Portfolio.pdf");
 });
