7c66bb357819ed5de12cbd34c5e77964918ef25a
Agustin Gomez <agustingomez1986@hotmail.com>
2022-08-19 02:03:29 -0300

    Agregado exportacion pdf

--- assets/js/convertir-pdf.js
+++ assets/js/convertir-pdf.js
@@ -1,40 +1,12 @@
-/*function convertirHTMLaPDF(){
-    const { jsPDF } = window.jspdf;
-    var pdf = new jsPDF('l', 'mm', [1200, 1210]);
-    var pdfjs = document.querySelector('#previewHTMLContent');
-    pdf.html(pdfjs, {
-        callback: function(pdf){
-            pdf.save("output.pdf");
-        },
-        x:10,
-        y:10
+const curriculoPDF = document.querySelector("#banner__redes__curriculo");
+curriculoPDF.addEventListener("click", async function(){
+    var downloading = document.body;
+    var doc = new jsPDF('p', 'pt', [1000, 3500]);
+    await html2canvas(downloading, {
+        allowTaint: true,
+        useCORS: true,
+    }).then((canvas)=>{
+        doc.addImage(canvas.toDataURL("image/jpg"), 'JPG', 0, 0, 1000, 3500);
     });
-}*/
-
-document.addEventListener("DOMContentLoaded", () => {
-    const $curriculo = document.querySelector("#banner__redes__curriculo");
-    $curriculo.addEventListener("click", () => {
-        const $elementoParaConvertir = document.body;
-        html2pdf()
-        .set({
-            margin: 1,
-            filename: 'Agustin_Gomez_Portfolio.pdf',
-            image: {
-                type: 'jpeg',
-                quality: 1
-            },
-            html2canvas: {
-                scale: 1,
-                letterRendering: true,
-            },
-            jdPDF: {
-                unit: "in",
-                format: "a4",
-                orientation: 'portrait'
-            }
-        })
-        .from($elementoParaConvertir)
-        .save()
-        .catch(err => console.log(err));
-    });
+    doc.save("Agustin_Gomez_Portfolio.pdf");
 });
