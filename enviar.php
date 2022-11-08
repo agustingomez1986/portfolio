4969b9a47f1b252ecaca9d4a0e11b8dbb6acadb8
Agustin Gomez <agustingomez1986@hotmail.com>
2022-11-08 03:11:27 -0300

    Mail metodo jyg

--- enviar.php
+++ enviar.php
@@ -1,21 +1,23 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$asunto = $_POST['asunto'];
+$name = $_POST['nombre'];
+// $email = $_POST['email'];
+// $asunto = $_POST['asunto'];
 $mensaje = $_POST['mensaje'];
 
-$header = 'Form: ' . $email . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Context-Type: text/plain";
+// $header = 'Form: ' . $email . " \r\n";
+// $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+// $header .= "Mime-Version: 1.0 \r\n";
+// $header .= "Context-Type: text/plain";
 
-$message = "Este mensaje fue enviado por: " . $name . " \r\n";
-$message .= "Su email es: " . $email . " \r\n";
-$message .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+// $message = "Este mensaje fue enviado por: " . $name . " \r\n";
+// $message .= "Su email es: " . $email . " \r\n";
+// $message .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+
+$contenido = "Nombre: " . $name . "\nEmail: " . $mensaje;
 
 $para = 'agustingomez1986@hotmail.com';
-$asunto = "Asunto: " . $asunto . " \r\n";
+// $asunto = "Asunto: " . $asunto . " \r\n";
 
-mail($para, $asunto, utf8_decode($message), $header);
+mail($para, "Contactooooo", $contenido);
 header("Location:index.html");
 ?>
